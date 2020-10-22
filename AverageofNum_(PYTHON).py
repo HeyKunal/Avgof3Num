9f574c3b6ebad77d02de38f6066ec7b547437619
HeyKunal <60438567+HeyKunal@users.noreply.github.com>
2020-10-22 11:41:01 +0530

    Add files via upload

---
+++ AverageofNum_(PYTHON).py
@@ -0,0 +1,11 @@
+print("enter any 3 numbers to find average: ");
+n1 = input();
+n2 = input();
+n3 = input();
+number1 = int(n1);
+number2 = int(n2);
+number3 = int(n3);
+sum = number1 + number2 + number3 
+a = sum/3;
+print("Average of entered 3 numbers is",a);
+# by Kunal Vartak (72)
